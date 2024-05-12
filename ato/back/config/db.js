366504bb46d081baec978b78f3516f917401840c
Eliot2202 <eliot@ezway-technology.com>
2024-05-12 13:18:57 +0300

    eliot

--- ato/back/config/db.js
+++ ato/back/config/db.js
@@ -1,7 +1,7 @@
 const {Sequelize} = require('sequelize');
 
 //Initalisation de la base de donnée
-const sequelize = new Sequelize('mysql://127.0.0.1:3306/react-native', { 
+const sequelize = new Sequelize('mysql://127.0.0.1:3306/react_native', { 
   username : 'root', 
   password :'',
   charset: 'utf8mb4'
